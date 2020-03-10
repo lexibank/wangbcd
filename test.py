07afe6df31330ea81a63d139ca0cdc329cda4b14
chrzyki <christoph@functor-argument.net>
2020-03-10 08:47:29 +0100

    Add Travis and pytest-cldf configuration

---
+++ test.py
@@ -0,0 +1,15 @@
+def test_valid(cldf_dataset, cldf_logger):
+    assert cldf_dataset.validate(log=cldf_logger)
+
+
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 5584
+    assert any(f["Form"] == "全部" for f in cldf_dataset["FormTable"])
+
+
+def test_parameters(cldf_dataset):
+    assert len(list(cldf_dataset["ParameterTable"])) == 200
+
+
+def test_languages(cldf_dataset):
+    assert len(list(cldf_dataset["LanguageTable"])) == 24
